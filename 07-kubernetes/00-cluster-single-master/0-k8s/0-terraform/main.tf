00e3d2ba984f86f711fc6dedb9514d47294a8253
Hugo Ernandes <hugo.ernandes@hotmail.com>
2021-10-19 14:44:58 +0000

    atualizando terraform de criacao k8s

--- 07-kubernetes/00-cluster-single-master/0-k8s/0-terraform/main.tf
+++ 07-kubernetes/00-cluster-single-master/0-k8s/0-terraform/main.tf
@@ -8,20 +8,26 @@ data "http" "myip" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners = ["099720109477"] # ou ["099720109477"] ID master com permissão para busca
+  owners      = ["099720109477"] # ou ["099720109477"] ID master com permissão para busca
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-*"] # exemplo de como listar um nome de AMI - 'aws ec2 describe-images --region us-east-1 --image-ids ami-09e67e426f25ce0d7' https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-images.html
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server*"] # exemplo de como listar um nome de AMI - 'aws ec2 describe-images --region us-east-1 --image-ids ami-09e67e426f25ce0d7' https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-images.html
   }
 }
 
 resource "aws_instance" "maquina_master" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami = data.aws_ami.ubuntu.id
+  root_block_device {
+    encrypted   = true
+    volume_size = 8
+  }
+  associate_public_ip_address = true
+  subnet_id     = "subnet-0926d1d3dff8224c3"
   instance_type = "t2.medium"
-  key_name      = "treinamento-turma1_itau"
+  key_name      = "key-outdev-hugo"
   tags = {
-    Name = "maquina-cluster-kubernetes-master"
+    Name = "hugo-maquina-cluster-kubernetes-master"
   }
   vpc_security_group_ids = ["${aws_security_group.acessos_master.id}"]
   depends_on = [
@@ -30,47 +36,54 @@ resource "aws_instance" "maquina_master" {
 }
 
 resource "aws_instance" "workers" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami = data.aws_ami.ubuntu.id
+  root_block_device {
+    encrypted   = true
+    volume_size = 8
+  }
+  associate_public_ip_address = true
+  subnet_id     = "subnet-0926d1d3dff8224c3"
   instance_type = "t2.micro"
-  key_name      = "treinamento-turma1_itau"
+  key_name      = "key-outdev-hugo"
   tags = {
-    Name = "maquina-cluster-kubernetes-${count.index}"
+    Name = "hugo-maquina-cluster-kubernetes-${count.index}"
   }
   vpc_security_group_ids = ["${aws_security_group.acessos_workers.id}"]
-  count         = 2
+
+  count = 2
 }
 
 
 resource "aws_security_group" "acessos_master" {
   name        = "acessos_master"
   description = "acessos_workers inbound traffic"
-
+  vpc_id      = "vpc-0d48f0e5799777077"
   ingress = [
     {
       description      = "SSH from VPC"
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids = null,
-      security_groups: null,
-      self: null
+      prefix_list_ids  = null,
+      security_groups : null,
+      self : null
     },
     {
-      description      = "Libera porta kubernetes"
-      from_port        = 6443
-      to_port          = 6443
-      protocol         = "tcp"
-      cidr_blocks      = [
+      description = "Libera porta kubernetes"
+      from_port   = 6443
+      to_port     = 6443
+      protocol    = "tcp"
+      cidr_blocks = [
         "${chomp(data.http.myip.body)}/32",
         "${aws_instance.workers[0].private_ip}/32",
         "${aws_instance.workers[1].private_ip}/32",
       ]
       ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids = null,
-      security_groups: null,
-      self: null
+      prefix_list_ids  = null,
+      security_groups : null,
+      self : null
     },
   ]
 
@@ -81,10 +94,10 @@ resource "aws_security_group" "acessos_master" {
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"],
-      prefix_list_ids = null,
-      security_groups: null,
-      self: null,
-      description: "Libera dados da rede interna"
+      prefix_list_ids  = null,
+      security_groups : null,
+      self : null,
+      description : "Libera dados da rede interna"
     }
   ]
 
@@ -97,18 +110,18 @@ resource "aws_security_group" "acessos_master" {
 resource "aws_security_group" "acessos_workers" {
   name        = "acessos_workers"
   description = "acessos_workers inbound traffic"
-
+  vpc_id      = "vpc-0d48f0e5799777077"
   ingress = [
     {
       description      = "SSH from VPC"
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids = null,
-      security_groups: null,
-      self: null
+      prefix_list_ids  = null,
+      security_groups : null,
+      self : null
     },
   ]
 
@@ -119,10 +132,10 @@ resource "aws_security_group" "acessos_workers" {
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"],
-      prefix_list_ids = null,
-      security_groups: null,
-      self: null,
-      description: "Libera dados da rede interna"
+      prefix_list_ids  = null,
+      security_groups : null,
+      self : null,
+      description : "Libera dados da rede interna"
     }
   ]
 
@@ -143,6 +156,6 @@ output "maquina_master" {
 output "aws_instance_e_ssh" {
   value = [
     for key, item in aws_instance.workers :
-      "worker ${key+1} - ${item.public_ip} - ssh -i ~/projetos/devops/id_rsa_itau_treinamento ubuntu@${item.public_dns}"
+    "worker ${key + 1} - ${item.public_ip} - ssh -i ~/projetos/devops/id_rsa_itau_treinamento ubuntu@${item.public_dns}"
   ]
 }
